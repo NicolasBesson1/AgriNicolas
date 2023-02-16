40678445bddec58140252b796068a57c5a1e1b62
NicolasBesson1 <58178659+NicolasBesson1@users.noreply.github.com>
2023-02-16 17:21:06 +0100

    Update global_data.py

--- python/global_data.py
+++ python/global_data.py
@@ -2,4 +2,7 @@ from initialize_data import *
 
 co2_consumption, farm_names, distances, points = initialize_data()
 
-
+production = [1 for _ in co2_consumption[1:]]
+truck_capacity = 10
+n=len(co2_consumption)
+N=[i for i in range(n)]
