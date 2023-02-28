c2a233410fefaed665901e6090f71a092674f137
NicolasBesson1 <58178659+NicolasBesson1@users.noreply.github.com>
2023-02-28 16:42:39 +0100

    Update global_data.py

--- python/global_data.py
+++ python/global_data.py
@@ -3,8 +3,8 @@ from random import randint
 
 co2_consumption, farm_names, distances, points = initialize_data()
 
-production = [1 for _ in co2_consumption[1:]]
+production = [1 for _ in co2_consumption]
 truck_capacity = 10
 n = len(co2_consumption)
 locations = [i for i in range(n)]
-opening_costs = [15 + randint(0, 10) for _ in range(n)]
+opening_costs = [15 for _ in range(n)]
