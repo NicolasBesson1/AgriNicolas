070e51d27d0403024735801aeab3ab45e6468f9f
NicolasBesson1 <58178659+NicolasBesson1@users.noreply.github.com>
2023-02-28 16:42:17 +0100

    Update test.py

--- python/test.py
+++ python/test.py
@@ -2,9 +2,11 @@ from global_data import *
 from display_results_tkinter import *
 from cvrp_solver import cvrp_solver
 from flp_solver import flp_solver
+from lrp_solver import lrp_solver
 
 if __name__ == '__main__':
     locations = [i for i in range(len(co2_consumption))]
+    '''
     solver = cvrp_solver(locations, co2_consumption, truck_capacity, production)
     print("Optimal value", solver.solve_optimal(fairness=True))
     print("Optimal route", solver.routes)
@@ -15,7 +17,14 @@ if __name__ == '__main__':
     solver_ufl = flp_solver(locations,co2_consumption,opening_costs)
     print("Optimal value", solver_ufl.solve_optimal(fairness=True))
     print("Optimal allocation", solver_ufl.cost_allocation)
+    '''
+    print("LRP :")
+    solver_lrp = lrp_solver(locations, opening_costs, co2_consumption, truck_capacity, production)
+    print("Optimal value", solver_lrp.solve_optimal(fairness=True))
+    print("Optimal allocation", solver_lrp.cost_allocation)
 
-    display_facilities(solver_ufl.opened_facilities,solver_ufl.serving_matrix,locations)
+    display_tour_facilities(solver_lrp.opened_facilities, solver_lrp.routes,locations)
 
-    
+
+    #display_facilities(solver_lrp.opened_facilities,solver_lrp.serving_matrix,locations)
+    #display_routes(solver_lrp.routes)
