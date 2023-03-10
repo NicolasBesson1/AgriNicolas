0f500556f8ddd39ed8a3ad1d0cf58fd3f41d3d8d
NicolasBesson1 <58178659+NicolasBesson1@users.noreply.github.com>
2023-03-10 17:02:02 +0100

    Update lrp_solver.py

--- python/lrp_solver.py
+++ python/lrp_solver.py
@@ -1,6 +1,6 @@
 
 from mip import *
-
+from core_solver import core_solver
 
 class lrp_solver:
 
@@ -13,15 +13,16 @@ class lrp_solver:
         self.demand = demand
         self.cost_allocation = None
         self.objective_function = 0
-
         self.opened_facilities = None
         self.serving_matrix = None
         self.cost_allocation = None
+        self.ir = None
+        self.m = None
+        self.optimal_cost = -1
 
-    def add_lrp_objective_function(self, model, x, o):
+    def add_lrp_objective_function(self, locations, model, x, o):
         f = self.location_costs
         d = self.arc_costs
-        locations = self.locations
         n = len(locations)
         self.objective_function = xsum(o[locations.index(i)] * f[i]
                                        for i in locations) + \
@@ -33,15 +34,13 @@ class lrp_solver:
 
     # Procedure :
     # Adds to model the LRP constraints
-    def add_lrp_constraints(self, model, x, y, o):
+    def add_lrp_constraints(self, locations, model, x, y, o):
         c = self.arc_costs
         f = self.location_costs
-        locations = self.locations
 
         M = sum(c[i][j] for i in locations for j in locations) + sum(f)
         n = len(locations)
 
-        locations = self.locations
         demand = self.demand
 
         for j in locations:
@@ -79,22 +78,20 @@ class lrp_solver:
             model += xsum(x[locations.index(i)][locations.index(j)][t] for j in locations) \
                      >= xsum(x[locations.index(i)][locations.index(j)][t+1] for j in locations)
 
-    def add_efficiency_constraint(self, model, cost):
-        locations = self.locations
+    def add_efficiency_constraint(self, locations, model, cost):
         model += self.objective_function == xsum(cost[i] for i in locations)
 
-    def add_individual_rationality(self, model, cost):
-        locations = self.locations
+    def add_individual_rationality(self, locations, model, cost):
         for j in locations:
             model += cost[locations.index(j)] <= round(self.location_costs[j], ndigits=1)
 
-    def add_incremental_contribution(self, model, cost):
+    def add_incremental_contribution(self, locations, model, cost):
         # Compute marginal contributions of each location :
-        tmp_solver = lrp_solver(self.locations, self.location_costs, self.arc_costs, self.capacity, self.demand)
+        tmp_solver = lrp_solver(locations, self.location_costs, self.arc_costs, self.capacity, self.demand)
         optimal = tmp_solver.solve_optimal()
 
         for j in self.locations:
-            tmp_solver = lrp_solver([i for i in self.locations if i != j], self.location_costs, self.arc_costs,
+            tmp_solver = lrp_solver([i for i in locations if i != j], self.location_costs, self.arc_costs,
                                     self.capacity, self.demand)
             mj = round(optimal - tmp_solver.solve_optimal(), ndigits=1)
             model += cost[self.locations.index(j)] >= mj
@@ -109,9 +106,8 @@ class lrp_solver:
     #           -Costs are allocated respecting efficiency, kickback, individual rationality
     #           and incremental contribution
 
-    def construct_routes(self, tours_matrix):
-        n = len(self.locations)
-        locations = self.locations
+    def construct_routes(self, locations, tours_matrix):
+        n = len(locations)
         routes = []
 
         for t in tours_matrix:
@@ -136,9 +132,11 @@ class lrp_solver:
 
         return routes
 
-    def solve_optimal(self, verbose=False, fairness=False):
+    def solve_optimal(self, locations = None, verbose=False, fairness=False):
 
-        locations = self.locations
+        if locations == None:
+            locations = self.locations
+
         n = len(locations)
 
         model = Model(solver_name=CBC)
@@ -156,31 +154,70 @@ class lrp_solver:
             cost = [model.add_var() for _ in locations]
 
         # Objective : minimize total cost of the tour
-        self.add_lrp_objective_function(model, x, o)
-
-        self.add_lrp_constraints(model, x, y, o)
+        self.add_lrp_objective_function(locations, model, x, o)
+        self.add_lrp_constraints(locations, model, x, y, o)
 
         if fairness:
-            self.add_efficiency_constraint(model, cost)
-            self.add_individual_rationality(model, cost)
-            self.add_incremental_contribution(model, cost)
+            self.add_efficiency_constraint(locations, model, cost)
+            self.add_individual_rationality(locations, model, cost)
+            self.add_incremental_contribution(locations, model, cost)
 
         model.optimize()
 
         # If feasible :
         if model.num_solutions:
-
-            self.opened_facilities = [int(o[locations.index(i)].x >= 0.99) for i in locations]
+            if self.opened_facilities == None:
+                self.opened_facilities = [int(o[locations.index(i)].x >= 0.99) for i in locations]
             # self.construct_routes(x,o)
             if fairness:
-                self.cost_allocation = [round(cost[locations.index(j)].x, ndigits=1) for j in locations]
+                if self.cost_allocation == None:
+                    self.cost_allocation = [round(cost[locations.index(j)].x, ndigits=1) for j in locations]
             # Return optimal value :
-            tours_matrix = [[[int(x[locations.index(i)][locations.index(j)][t].x >= 0.99)
-                            for j in locations] for i in locations] for t in range(n)]
-            self.routes = self.construct_routes(tours_matrix)
-            return round(model.objective_value, ndigits=1)
+            if self.routes == None:
+                tours_matrix = [[[int(x[locations.index(i)][locations.index(j)][t].x >= 0.99)
+                                for j in locations] for i in locations] for t in range(n)]
+                self.routes = self.construct_routes(locations, tours_matrix)
+            self.optimal_cost = round(model.objective_value, ndigits=1)
+            return self.optimal_cost
 
         else:
             if verbose:
                 print('Not feasible')
             assert False
+
+    def core_procedure(self, verbose=False):
+        csolver = core_solver(self.locations,self.solve_optimal)
+        self.cost_allocation = csolver.core_procedure()
+        return self.cost_allocation
+
+    def individual_rationnalities(self):
+        locations = self.locations
+        ir = []
+        for l in locations:
+            ir.append(self.location_costs[locations.index(l)])
+        self.ir = ir
+        return ir
+
+    def incremental_contribution(self):
+        locations = self.locations
+        # Compute marginal contributions of each location :
+        tmp_solver = lrp_solver(locations, self.location_costs, self.arc_costs, self.capacity, self.demand)
+        optimal = tmp_solver.solve_optimal()
+        m = []
+        for j in self.locations:
+            tmp_solver = lrp_solver([i for i in locations if i != j], self.location_costs, self.arc_costs,
+                                    self.capacity, self.demand)
+            m.append(round(optimal - tmp_solver.solve_optimal(), ndigits=1))
+
+        self.m = m
+        return m
+
+
+    def proportional_allocation(self):
+        locations = self.locations
+        ir = self.individual_rationnalities()
+        sir = sum(ir)
+        self.cost_allocation=[]
+        for location in locations:
+            self.cost_allocation.append(round((ir[locations.index(location)]/sir)*self.optimal_cost, ndigits=1))
+        return self.cost_allocation
